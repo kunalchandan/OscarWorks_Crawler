4db70e00323d8d4f82bb863282f33f484db7003d
Kunal Chandan <kunal_chandan@outlook.com>
2019-02-04 02:37:00 -0500

    Update Oscar Crawler, works completely on McMaster

--- crawl.py
+++ crawl.py
@@ -29,7 +29,7 @@ def define_parser():
                      metavar='LOGIN_FILE')
     parser.add_argument('-o', '--output-folder',
                      help='The output folder, default value is "query_find/".',
-                     default='query_find/',
+                     default='query_find\\',
                      metavar='OUTPUT_FOLDER')
     req.add_argument('-m', '--mcmaster',
                      help='Flag specifies to log into McMaster\'s OscarPlus',
@@ -83,6 +83,11 @@ def go_to_nth_page(web_driver, counter):
         time.sleep(.5)
 
 
+def go_to_postings(web_driver):
+    web_driver.get(oscar_postings_url)
+    web_driver.find_element_by_link_text("View all available postings").click()
+
+
 def login(user_id, pin, web_driver):
     field = web_driver.find_element_by_id("user_id")
     field.send_keys(user_id)
@@ -91,11 +96,6 @@ def login(user_id, pin, web_driver):
     field.send_keys(pin)
 
     field.send_keys(Keys.RETURN)
-
-
-def go_to_postings(web_driver):
-    web_driver.get(oscar_postings_url)
-    web_driver.find_element_by_link_text("View all available postings").click()
 
 
 def gather_postings(web_driver):
@@ -112,12 +112,32 @@ def get_total_postings(driver):
     return int(total.text)
 
 
+def save_posting(args, description, job_id, job_name):
+    try:
+        location = '{}\\{}{}-{}.txt'.format(os.path.dirname(os.path.realpath(__file__)),
+                                            args.output_folder,
+                                            job_id,
+                                            job_name)
+        output = open(location, 'w+')
+        output.write(description)
+        output.close()
+    except IOError:
+        try:
+            os.mkdir('{}\\{}'.format(os.path.dirname(os.path.realpath(__file__)), args.output_folder))
+        except (IOError, OSError):
+            print 'ERROR:: There was some problem in making the output folder? Please create it manually and/or make ' \
+                  'sure you have Read/Write Access'
+            raise OSError('Refer to message above')
+
+
 def main():
     args = define_parser().parse_args()
     if args.mcmaster:
         driver = define_driver_oscar()
     if args.waterloo:
         driver = define_driver_works()
+    if not (args.mcmaster or args.waterloo):
+        raise SyntaxError('You have not specified which site to log into')
     # TODO:: Complete defintions for waterloo works
     # TODO:: Consider redesigning code for parallel login of both systems
 
@@ -140,17 +160,22 @@ def main():
         matches = gather_postings(driver)
 
         # STEP THROUGH POSTINGS ON PAGE
-        for each in matches:
-            each = each[7:]
-            driver.get('{}?action=displayPosting&postingId={}&npfGroup='.format(oscar_postings_url, each))
+        for each_id in matches:
+            each_id = each_id[7:]
+            driver.get('{}?action=displayPosting&postingId={}&npfGroup='.format(oscar_postings_url, each_id))
             # SEE IF PAGE CONTAINS ANYTHING FROM KEYWORDS
             tables = driver.find_elements_by_css_selector("table[class='table table-bordered']")
+            job_name = driver.find_element_by_class_name('span7').text.replace('\n', ' ').strip('.')
             description = ''
             for table in tables:
                 description += '\n' + table.text.encode('ascii', 'ignore')
             if any(keys in description for keys in keywords):
                 # Save job to output folder
-                print 'lel, this job aight; Posting ID: {}'.format(each)
+                print 'lel, this job aight; Posting ID: {}'.format(each_id)
+                print job_name
+                job_name = (''.join(e for e in job_name if e.isalnum())).encode('ascii', 'ignore').strip()
+
+                save_posting(args, description, each_id, job_name)
         go_back = driver.find_element_by_link_text('Back to Search Results')
         go_back.send_keys(Keys.ENTER)
         counter += 1
